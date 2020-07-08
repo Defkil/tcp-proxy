b3ea70ff14c7daf2bb6389f0dca1e4996cd4ad85
Oliver <dev@olivergruettner.de>
2020-07-08 04:52:03 +0200

    fixing git auth

--- gendoc.sh
+++ gendoc.sh
@@ -29,6 +29,6 @@ git config user.email "travis@travis-ci.org"
 
 git add --all
 git commit -m "Deploy code docs to GitHub Pages Travis build: $TRAVIS_BUILD_NUMBER" -m "Commit: $TRAVIS_COMMIT"
-git push --force "https://cc3b1e565535811a9af912b1390effc9ff48a34b@github.com/${TRAVIS_REPO_SLUG}.git"
+git push --force "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
 
 echo "Documentation successfully published"
